d505f4059e7d4d2fdfcadf2e78f71beae82e90f5
sljohnson32 <sljohnson32@gmail.com>
2017-02-14 08:53:03 -0700

    update build folder to public

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: './app',
   output: {
-    path: './build',
+    path: './public',
     filename: 'bundle.js'
   },
   module: {
@@ -17,7 +17,7 @@ module.exports = {
     ]
   },
   devServer: {
-    contentBase: './build',
+    contentBase: './public',
     inline: true
   },
   resolve: {
