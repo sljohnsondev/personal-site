32af58ba4ff86ed3011a79d960bfaf02d3a6c86f
sljohnson32 <sljohnson32@gmail.com>
2017-02-14 08:57:27 -0700

    final build for production push test

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: './app',
   output: {
-    path: './public',
+    path: './docs',
     filename: 'bundle.js'
   },
   module: {
@@ -17,7 +17,7 @@ module.exports = {
     ]
   },
   devServer: {
-    contentBase: './public',
+    contentBase: './docs',
     inline: true
   },
   resolve: {
