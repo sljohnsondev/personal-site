b260e98a8646c8ba1dd8946a9dfc2f4b6e5b5e08
sljohnson32 <sljohnson32@gmail.com>
2017-02-13 22:29:05 -0700

    Add code formatting and html text

--- app/components/Code/index.js
+++ app/components/Code/index.js
@@ -7,6 +7,26 @@ const Code = () => {
       <a className='nav-hook' name='code'></a>
       <h2>Code</h2>
       <h4>Some projects I've worked on</h4>
+      <section className='projects'>
+        <article className='project'>
+          <img src={require('../../Images/project-image.jpg')} />
+          <h3>Shoot The Breeze</h3>
+          <h5>Authenticated chat app built on top of Firebase</h5>
+          <h5 className='tech'>React.js - Firebase - Enzyme/Chai/Sinon</h5>
+        </article>
+        <article className='project'>
+          <img src={require('../../Images/project-image.jpg')} />
+          <h3>Weatherly</h3>
+          <h5>Weather forecast app built on external APIs</h5>
+          <h5 className='tech'>React.js - Redux - External API - Sass - Enzyme/Chai</h5>
+        </article>
+        <article className='project last-project'>
+          <img src={require('../../Images/project-image.jpg')} />
+          <h3>Chuckle Norris Joke Machine</h3>
+          <h5>Random joke generator built on external API</h5>
+          <h5 className='tech'>React.js - React Router - External API - Sass - Enzyme/Chai</h5>
+        </article>
+      </section>
     </div>
   );
 }
