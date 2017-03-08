18ea3bb5d64dada15f01eba55f9f71dc77009bee
sljohnson32 <sljohnson32@gmail.com>
2017-03-08 11:36:46 -0700

    css for mobile

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -5,14 +5,16 @@ import Nav from '../Nav';
 const Header = () => {
   return (
     <div className='section-header'>
-      <a href='#home'><section className='icon-info-container'>
-          <img className='icon-img' src={require('../../Images/sj-icon.png')} />
-        <article className='sj-info'>
-          <h1>Sam Johnson</h1>
-          <h3>Front-End Developer</h3>
-        </article>
-      </section></a>
-      <Nav />
+      <section className='header-container'>
+        <a href='#home'><section className='icon-info-container'>
+            <img className='icon-img' src={require('../../Images/sj-icon.png')} />
+          <article className='sj-info'>
+            <h1>Sam Johnson</h1>
+            <h3>Front-End Developer</h3>
+          </article>
+        </section></a>
+        <Nav />
+      </section>
     </div>
   );
 }
