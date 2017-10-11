b5ef708e8baad5849e11b6163a93b2fe39b8a4e7
sljohnson32 <sljohnson32@gmail.com>
2017-10-10 21:53:35 -0600

    Arrow toggling on click

--- app/components/Resume/index.js
+++ app/components/Resume/index.js
@@ -17,7 +17,7 @@ export default class Resume extends React.Component {
   render() {
     return (
       <div className='resume-container'>
-        <a className='resume-button' onClick={ () => this.toggleResume() }><h3>Check out my resume {this.state.showResume ? <img src='app/Images/uparrow.png' /> : <img src='../../Images/downarrow.png' />}</h3></a>
+        <a className='resume-button' onClick={ () => this.toggleResume() }><h3>Check out my resume {this.state.showResume ? <img src={require('../../Images/uparrow.png')} /> : <img src={require('../../Images/downarrow.png')} />}</h3></a>
         <iframe className='.resume-html' src="https://resume.creddle.io/embed/7tfjak0njst" width="850" height="1100" seamless></iframe>
       </div>
     );
