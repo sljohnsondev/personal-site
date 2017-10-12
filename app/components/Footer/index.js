c52fe5b00c567a42a8271c44ae8c8ccf28a15bc0
sljohnson32 <sljohnson32@gmail.com>
2017-10-11 20:37:41 -0600

    Testing new resume format

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -9,12 +9,8 @@ const Footer = () => {
       <section className='container-social'>
         <a className="social-media" href="https://github.com/sljohnson32" target="_blank">
           <img src={require("../../Images/social/github.png")} alt="Sam's GitHub profile"/></a> |
-        <a className="social-media" href="https://www.facebook.com/sljohnson32" target="_blank">
-          <img src={require("../../Images/social/facebook.png")} alt="Sam's Facebook profile"/></a> |
         <a className="social-media" href="https://www.instagram.com/sljohnson_" target="_blank">
           <img src={require("../../Images/social/instagram.png")} alt="Sam's Instagram profile"/></a> |
-        <a className="social-media" href="https://www.twitter.com/sjohnson_" target="_blank">
-          <img src={require("../../Images/social/twitter.png")} alt="Sam's Twitter profile"/></a> |
         <a className="social-media" href="https://www.linkedin.com/in/sljohnson" target="_blank">
           <img src={require("../../Images/social/linkedin.png")} alt="Sam's LinkedIn profile"/></a> |
           <a className="social-media" href="mailto:sljohnson32@gmail.com">
--- app/components/Resume/index.js
+++ app/components/Resume/index.js
@@ -18,7 +18,11 @@ export default class Resume extends React.Component {
     return (
       <div className='resume-container'>
         <a className='resume-button' onClick={ () => this.toggleResume() }><h3>Check out my resume {this.state.showResume ? <img src={require('../../Images/uparrow.png')} /> : <img src={require('../../Images/downarrow.png')} />}</h3></a>
-        <iframe className='.resume-html' src="https://resume.creddle.io/embed/7tfjak0njst" width="850" height="1100" seamless></iframe>
+        {this.state.showResume ?
+          <iframe className='resume-show' src="https://drive.google.com/file/d/0B1LF3i2gTJgNc0tmcTNYenlhWU0/preview" width="700" height="900" seamless></iframe>
+          :
+          <div className='resume-hide' />
+        }
       </div>
     );
   }
