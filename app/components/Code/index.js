e47a50a2b1d3c003afe77080c4013a25d08d0368
sljohnson32 <sljohnson32@gmail.com>
2017-11-14 10:31:22 -0700

    Update stack info in code section

--- app/components/Code/index.js
+++ app/components/Code/index.js
@@ -13,7 +13,7 @@ const Code = () => {
           <article className='project-text'>
             <h3>School Finder</h3>
             <h5>Google Maps web app for parents looking for nearby schools in Denver</h5>
-            <h5 className='tech'>React.js, Redux, Postgres w/ Express & Knex, Google Maps APIs, Enzyme/Chai/Chai HTTP</h5>
+            <h5 className='tech'>React, Redux, Node, Express, Knex, PostgreSQL, Google Maps APIs, Enzyme/Chai/Chai HTTP</h5>
             <div className='project-links'>
               <a className='project-link' href='https://github.com/sljohnson32/school-finder' target="_blank"><img src={require('../../Images/social/github.png')} /></a>
               <a className='project-link' href='https://school-finder-86e85.firebaseapp.com/' target="_blank"><img src={require('../../Images/social/browser.png')} /></a>
@@ -24,8 +24,8 @@ const Code = () => {
           <img className='project-img' src={require('../../Images/projects/grad-cap.png')} />
           <article className='project-text'>
             <h3>Colorado Education Data API</h3>
-            <h5>Open API with publicly available, school-based education data</h5>
-            <h5 className='tech'>Postgres w/ Express & Knex, Chai/Chai HTTP</h5>
+            <h5>RESTful API with publicly available, school-based education data</h5>
+            <h5 className='tech'>Node, Express, Knex, PostgreSQL, Chai/Chai HTTP</h5>
             <div className='project-links'>
               <a className='project-link' href='https://github.com/sljohnson32/CDoE_data_api' target="_blank"><img src={require('../../Images/social/github.png')} /></a>
               <a className='project-link' href='https://cdoe-data-api.herokuapp.com/' target="_blank"><img src={require('../../Images/social/browser.png')} /></a>
@@ -37,7 +37,7 @@ const Code = () => {
           <article className='project-text'>
             <h3>Shoot The Breeze</h3>
             <h5>Authenticated chat app built on top of Firebase</h5>
-            <h5 className='tech'>React.js, Firebase, Enzyme/Chai/Sinon</h5>
+            <h5 className='tech'>React, Firebase, Enzyme/Chai/Sinon</h5>
             <div className='project-links'>
               <a className='project-link' href='https://github.com/sljohnson32/shoot-the-breeze' target="_blank"><img src={require('../../Images/social/github.png')} /></a>
               <a className='project-link' href='https://shoot-the-breeze-8b841.firebaseapp.com/' target="_blank"><img src={require('../../Images/social/browser.png')} /></a>
