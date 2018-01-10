4981b36ce89646996c8a2e822a5d9536a6d83c03
sljohnson32 <sljohnson32@gmail.com>
2018-01-10 15:41:37 -0700

    Update header and about text FE to Software Engineer

--- app/components/About/index.js
+++ app/components/About/index.js
@@ -10,7 +10,7 @@ const About = () => {
       <p className='about-bold'>At my core, I am a pragmatic problem solver.</p>
       <p>I spent the first 11 years of my career at Teach For America building operational capacity through smart solutions to business problems.</p>
       <p>During my last five years at TFA, I worked as a product owner and team leader for Technology Solutions.</p>
-      <p className='about-bold'>My skills as a front-end software developer allow me to be a more dangerous problem solver and solutions ninja.</p>
+      <p className='about-bold'>Having recently completed the Front-End Engineering program at Turing, my skills as a software engineer allow me to be a more dangerous problem solver and solutions ninja.</p>
       <h3 className='resume-link'><a href="https://drive.google.com/file/d/0B1LF3i2gTJgNR3p4ZzVZSndYY0E/view?usp=sharing" target='_blank'>Check out my resume </a><img className='icon-arrow' src={require('../../Images/resume-icon.png')} /></h3>
     </div>
   );
