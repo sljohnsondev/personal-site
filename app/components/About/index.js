3e65fe50f70007274d55ca839bd3b1ba5140c189
sljohnson32 <sljohnson32@gmail.com>
2018-01-27 15:11:03 -0700

    Update about me text

--- app/components/About/index.js
+++ app/components/About/index.js
@@ -5,12 +5,12 @@ const About = () => {
   return (
     <div className='section-about main'>
       <a className='nav-hook' name='about'></a>
-      <h2>About</h2>
+      <h2>About Me</h2>
       <h4></h4>
       <p className='about-bold'>At my core, I am a pragmatic problem solver.</p>
-      <p>I spent the first 11 years of my career at Teach For America building operational capacity through smart solutions to business problems.</p>
-      <p>During my last five years at TFA, I worked as a product owner and team leader for Technology Solutions.</p>
-      <p className='about-bold'>Having recently completed the Front-End Engineering program at Turing, my skills as a software engineer allow me to be a more dangerous problem solver and solutions ninja.</p>
+      <p>I am motivated by the challenge that a new problem presents and the learning and growth that comes from solving it.</p>
+      <p>Before studying in the FE Engineering program at Turing, I was a product manager tasked with collaborating with developers to figure out how to translate my end-users' needs into effective technology solutions.</p>
+      <p className='about-bold'>Being a software developer has reignited my passion to learn and grow in my career precisely because of the hard-skills it gives me to do what I do best and care about most...building things that solve problems.</p>
       <h3 className='resume-link'><a href="https://drive.google.com/file/d/0B1LF3i2gTJgNR3p4ZzVZSndYY0E/view?usp=sharing" target='_blank'>Check out my resume </a><img className='icon-arrow' src={require('../../Images/resume-icon.png')} /></h3>
     </div>
   );
