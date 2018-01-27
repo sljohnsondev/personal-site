c2fbd6a4055b490554f3fa30c416f92e21509bbd
sljohnson32 <sljohnson32@gmail.com>
2018-01-27 15:28:29 -0700

    Small text changes to About sections and minor CSS tweaks

--- app/components/About/index.js
+++ app/components/About/index.js
@@ -6,11 +6,10 @@ const About = () => {
     <div className='section-about main'>
       <a className='nav-hook' name='about'></a>
       <h2>About Me</h2>
-      <h4></h4>
       <p className='about-bold'>At my core, I am a pragmatic problem solver.</p>
       <p>I am motivated by the challenge that a new problem presents and the learning and growth that comes from solving it.</p>
-      <p>Before studying in the FE Engineering program at Turing, I was a product manager tasked with collaborating with developers to figure out how to translate my end-users' needs into effective technology solutions.</p>
-      <p className='about-bold'>Being a software developer has reignited my passion to learn and grow in my career precisely because of the hard-skills it gives me to do what I do best and care about most...building things that solve problems.</p>
+      <p>Before enrolling in the FE program at Turing, I was a product manager that collaborated closely with developers to figure out how best to translate my end-users' needs into effective technology solutions.</p>
+      <p className='about-bold'>Becoming a software engineer has reignited my passion to learn and grow in my career precisely because of the hard-skills it gives me to do what I do best and care about most...building things that solve problems.</p>
       <h3 className='resume-link'><a href="https://drive.google.com/file/d/0B1LF3i2gTJgNR3p4ZzVZSndYY0E/view?usp=sharing" target='_blank'>Check out my resume </a><img className='icon-arrow' src={require('../../Images/resume-icon.png')} /></h3>
     </div>
   );
