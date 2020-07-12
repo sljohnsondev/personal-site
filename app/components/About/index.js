5ed58015cd9d9132d72ddd53cb73715700a927e6
sljohnson32 <sljohnson32@gmail.com>
2020-07-12 13:53:17 -0600

    updates text alignment and text size

--- app/components/About/index.js
+++ app/components/About/index.js
@@ -10,7 +10,7 @@ const About = () => {
       <p>I am motivated by the challenge that a new problem presents and the learning and growth that comes from solving it.</p>
       <p>Before enrolling in the FE program at Turing and starting my career as a software engineer, I was a product manager that collaborated closely with developers to figure out how best to translate my end-users' needs into effective technology solutions.</p>
       <p className='about-bold'>Becoming a software engineer has reignited my passion to learn and grow in my career precisely because of the hard-skills it gives me to do what I do best and care about most...building things that solve problems.</p>
-      <h3 className='resume-link'><a href="https://drive.google.com/file/d/0B1LF3i2gTJgNR3p4ZzVZSndYY0E/view?usp=sharing" target='_blank' rel="noopener">Check out my resume.</a></h3>
+      <p className='resume-link about-bold'><a href="https://drive.google.com/file/d/0B1LF3i2gTJgNR3p4ZzVZSndYY0E/view?usp=sharing" target='_blank' rel="noopener">Check out my resume.</a></p>
     </div>
   );
 }
